bc8538b579677ca810ab66b34265181619da4ecb
V11bot <111274955+V11bot@users.noreply.github.com>
2022-08-30 18:37:07 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,15 +1,15 @@
 let handler = async m => conn.sendButton(m.chat, `
       ━━━━『 *DONASI KAK* 』━━━━
-➸ Dana : 0831-7588-6677
-➸ Gopay : 0831-7588-6677
-➸ Pulsa : 0831-7588-6677 
-➸ Saweria : https://saweria.co/3SBot
+➸ Dana : 0831-8332-1692
+➸ Gopay : 0831-8332-1692
+➸ Pulsa : 0831-8332-1692
+➸ Saweria : https://wa.me/p/6068742659806028/15012058590
 *➸ QRIS : Chat Owner*
 
 (Melanggar Rules Di Ban 1 hari) 
 Donasi supaya bot terus update & Aktif
 
-`.trim(), '© 3S~Bot', { 'button[0]': 'Owner', 'row[0]': '.owner'}, m, {}, 'location', 'https://telegra.ph/file/1149dc82a0cc9d2d8b0c5.jpg') // Tambah sendiri kalo mau
+`.trim(), '© ʙʟᴀᴄᴋ ʙᴏᴛ ᴏғᴄ', { 'button[0]': 'Owner', 'row[0]': '.owner'}, m, {}, 'location', 'https://telegra.ph/file/1149dc82a0cc9d2d8b0c5.jpg') // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['donasi']
 handler.command = /^dona(te|si)$/i
